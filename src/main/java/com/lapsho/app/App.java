533aadd22e41090c617edb3acf058d0ba87409ce
Andrew Pavlovsky <pavlowskiandrew@gmail.com>
2022-08-09 07:52:41 +0300

    feat: initiated project with maven

---
+++ src/main/java/com/lapsho/app/App.java
@@ -0,0 +1,12 @@
+package com.lapsho.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String stripComments(String text, String[] commentSymbols) {
+        return "";
+    }
+}
